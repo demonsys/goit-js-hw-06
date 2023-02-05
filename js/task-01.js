92a7457ca3c453fa04391d1d14411fd94b101531
demonsys <demonsysk@gmail.com>
2023-02-06 00:19:11 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,4 @@
 const categoriesList = document.querySelector("#categories").children;
-console.log(categoriesList);
 console.log("Number of categories: " + categoriesList.length);
 for (let c of categoriesList) {
   console.log("Category: ", c.querySelector("h2").textContent);
