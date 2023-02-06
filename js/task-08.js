0b96df8740a21092d692674f95c70c66941afe25
demonsys <demonsysk@gmail.com>
2023-02-06 23:30:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,11 +2,12 @@ const form = document.querySelector(".login-form");
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const { email, password } = event.currentTarget.elements;
-  if (email.value === "" || password.value === "") {
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  if (mail === "" || password === "") {
     alert("Все поля должны быть заполнены!");
   } else {
-    console.log({ [email.value]: password.value });
+    console.log({ mail, password });
     event.currentTarget.reset();
   }
 }
